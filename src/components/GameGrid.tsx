75a13b1c8d80781c2d48dad6debe69d237f785e9
Luong Dang <luongkhdang@gmail.com>
2024-08-21 20:39:55 -0700

    add genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,20 +2,31 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={3}
+      >
         {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
 
-        {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+        {data.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,11 @@
+import useData from "./useData";
+
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
+}
+
+const useGeneres = () => useData<Genre>("/genres");
+
+export default useGeneres;
