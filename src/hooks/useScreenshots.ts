d91260cb03679b864dbe753c0910bcb60f013742
Luong Dang <luongkhdang@gmail.com>
2024-08-31 23:46:44 -0700

    build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshot } from "../entities/Screenshot";
+import APIClient from "../services/api-client";
+
+const useScreenshots = (gameId: number) => {
+  const apiClinet = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClinet.getAll,
+  });
+};
+
+export default useScreenshots;
